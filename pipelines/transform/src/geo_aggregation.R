a272cb1986bd4b63536742603ea170a39205cdad
Jonathan St-Onge <jonathanstonge7@gmail.com>
2026-04-11 21:06:02 -0400

    higher up

--- pipelines/transform/src/geo_aggregation.R
+++ pipelines/transform/src/geo_aggregation.R
@@ -51,7 +51,7 @@ geo_aggregation <- function() {
   contours_path <- here("pipelines", "transform", "input", "contours.geojson")
   if (file.exists(dem_file)) {
     if (file.exists(contours_path)) file.remove(contours_path)
-    levels <- seq(50, 225, by = 25)
+    levels <- seq(100, 230, by = 10)
     system2("gdal_contour", c("-a", "elevation", "-fl", levels, dem_file, contours_path, "-f", "GeoJSON"))
     message(sprintf("Wrote contours to %s", contours_path))
   } else {
